6bead2de0bdcfd9a4fe3aef35ced4c27d510fe62
Jatheepan Kanthavel <theepan@cutedrops.com>
2017-09-22 08:39:05 -0400

    inital commit  for view single user

--- app/router.js
+++ app/router.js
@@ -2,18 +2,22 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('page-not-found', {
-    path: '/*wildcard'
-  });
-  this.route('users', function() {
-    this.route('new');
-  });
+    this.route('about');
+    this.route('page-not-found', {
+        path: '/*wildcard'
+    });
+    this.route('users', function() {
+        this.route('new');
+
+        this.route('view', {
+            path: 'view/:id'
+        });
+    });
 });
 
 export default Router;
