d457d29af47e224d85f8a965d9426ab5b68583a8
Jatheepan Kanthavel <theepan@cutedrops.com>
2017-09-22 05:50:54 -0400

    Added adapter

--- app/controllers/users/new.js
+++ app/controllers/users/new.js
@@ -3,9 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         saveUser() {
-            let values = this.getProperties('firstname', 'lastname');
-
-            console.log(values);
+        
         }
     }
 });
